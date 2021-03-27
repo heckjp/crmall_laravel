979087dda827f351b0305c6fb926f4a6f8505f9d
Hector Y. Nagao <hectornagao@hotmail.com>
2021-03-27 14:04:56 -0300

    recriação de migration para conserto de atributos da tabela clientes

---
+++ database/migrations/2021_03_27_164850_create_clientes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome',300)->nullable(false)->charset('latin1')->collation('latin1_swedish_ci');
+            $table->date('data_nascimento')->nullable(false);
+            $table->char('sexo',1)->nullable(false)->charset('latin1')->collation('latin1_swedish_ci');
+            $table->char('cep',8)->nullable()->charset('latin1')->collation('latin1_swedish_ci');
+            $table->string('endereco',300)->nullable()->charset('latin1')->collation('latin1_swedish_ci');
+            $table->string('complemento',200)->nullable()->charset('latin1')->collation('latin1_swedish_ci');
+            $table->string('bairro',300)->nullable()->charset('latin1')->collation('latin1_swedish_ci');
+            $table->string('cidade',300)->nullable()->charset('latin1')->collation('latin1_swedish_ci');
+            $table->char('estado',2)->nullable()->charset('latin1')->collation('latin1_swedish_ci');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
